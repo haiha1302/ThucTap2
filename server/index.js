0d600a8d851929580b82278496e45b0255d7b80c
Nguyen Hai Ha <td12a11haiha@gmail.com>
2022-07-26 10:28:25 +0700

    update index

--- server/index.js
+++ server/index.js
@@ -1,41 +1,59 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const fileUpload = require('express-fileupload')
-const { connectToMongo } = require("./database");
-const user = require("./routes/userRoutes");
-const post = require("./routes/postRoute");
+const express = require('express');
+const path = require('path')
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const { connectToMongo } = require('./database');
+const user = require('./routes/userRoutes');
+const post = require('./routes/postRoute');
+const { notFound, errorHandler } = require('./middleware/catchError');
 
 const app = express();
 
 //config
-dotenv.config()
+dotenv.config();
+
+// database
+connectToMongo();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-    // origin: '*',
-    origin: 'http://localhost:3000',
-    methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-    credentials: true,
-}));
-app.use(fileUpload({
-    useTempFiles: true
-}))
+app.use(
+    cors({
+        // origin: '*',
+        origin: 'http://localhost:3000',
+        methods: 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+        credentials: true,
+    }),
+);
+app.use(
+    fileUpload({
+        useTempFiles: true,
+    }),
+);
 
 app.use('/welcome', (req, res) => {
-    res.status(200).json('Message: Welcome!!!')
-})
+    res.status(200).json('Message: Welcome!!!');
+});
 
 //routes
-app.use("/user", user);
-app.use("/posts", post);
+app.use('/user', user);
+app.use('/posts', post);
 
-// database
-connectToMongo()
+// deployment configuration
+const __variableOfChoice = path.resolve();
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/client/build')));
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__variableOfChoice, 'client', 'build', 'index.html')));
+}
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port: ${PORT}`));
--- server/middleware/catchError.js
+++ server/middleware/catchError.js
@@ -1,6 +1,17 @@
-(theFunc) => (req, res, next) => {
-    Promise.resolve(theFunc(req, res, next))
-    .catch(next);
+const notFound = (err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        // deploy
+        stack: (process.env.NODE_ENV = 'production' ? null : err.stack),
+    });
 };
 
-module.exports = theFunc
+const errorHandler = (req, res, next) => {
+    const error = new Error(`Not found: ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+};
+
+module.exports = { notFound, errorHandler };
